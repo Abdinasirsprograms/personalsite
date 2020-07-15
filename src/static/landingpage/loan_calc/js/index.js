bd336bcbf5b7877c4469715e5f3e877befc101a1
Abdinasir <abdinasirnoor@outlook.com>
2020-07-15 15:38:39 -0400

    Arithmetic setup on loan calc; refactoring is next

--- src/static/landingpage/loan_calc/js/index.js
+++ src/static/landingpage/loan_calc/js/index.js
@@ -1,11 +1,38 @@
-function convert_two_spots(number){
+var principle, apr, term;
+var notBlank = false;
+function convertToTwoDecimalSpots(number){
     return (Math.round(number * 100)/100).toFixed(2);
 }
 // Amortized loan
 // Payment = Amount / Discount Facor
+function payments(Principle, DiscountFactor){
+    return convertToTwoDecimalSpots(Principle/DiscountFactor)
+}
 // n = # of payments per year * loan term length
-// r = APR/n
+function paymentsPerYear(termLength) {
+    let paymentPeriods = Math.round(termLength) * 12;
+    return paymentPeriods;
+}
+// r = APR/12
+function periodicInterestRate(APR) {
+    let rate = (parseFloat(APR)/100).toFixed(2);
+    return rate/12;
+}
 // D = {[(1 + r)n] - 1}/[r(1 + r)n]
+function discountFactor(rate, paymentPeriod){
+    let paymentPeriods = Math.round(paymentPeriod);
+    let rateAddOne = rate + 1;
+    let rateMulitpliedByPaymentPeriods = rateAddOne ** paymentPeriods;
+    let DiscountFactor = (rateMulitpliedByPaymentPeriods - 1)/(rate * rateMulitpliedByPaymentPeriods);
+    return DiscountFactor;
+}
+// Payments for just interest
+// Payment = Amount * (APR/12)
+function interestOnly(loanAmount, APR){
+    let YearlyAPR =  ((parseFloat(APR)/100).toFixed(2))/12
+    let Payments = Math.round(loanAmount * YearlyAPR)
+    return Payments
+}
 // example: loan of 10k at 3% for 7 years
 // n = 84 (12 monthly payments/year * 7 years)
 // r = 0.0025 (0.03/by 12 payments per year)
@@ -14,26 +41,86 @@ function convert_two_spots(number){
 // calculating the interest only payment on the above example:
 // Payment = Amount * (APR/12)
 // P = $25 (10,000 * (0.03/12))
+
 function listen_for_input(e) {
     switch(e.target.name){
         case 'term':
-            console.log("it's a term");
+            term = e.target.value;
             break;
         case 'principle':
-            console.log("it's a principle");
+            principle = e.target.value;
             break;
         case 'apr':
-            console.log("it's a apr");
+            apr = e.target.value;
             break;
         default:
             console.log('None')
             break;
     }
+    if((!term) || (!principle) || (!apr)){
+        notBlank = true;
+    }
+    else {
+        notBlank = false;
+    }
+    amortizedLoan()
     return
 }
 document.querySelector("input[name=principle]").addEventListener('input', listen_for_input);
 document.querySelector("input[name=apr]").addEventListener('input', listen_for_input);
 document.querySelector("input[name=term]").addEventListener('input', listen_for_input);
-var principle = 0;
-var apr = 0;
-var term = 0;
+
+
+function amortizedLoan() {
+    if (term > 0) {
+        var numberOfPayments = paymentsPerYear(term);
+    }
+    if (apr > 0) {
+        var periodicRate = periodicInterestRate(apr);
+        var calculatedDiscountFactor = discountFactor(periodicRate, numberOfPayments);
+    }
+    if (principle > 0 && (calculatedDiscountFactor !== undefined)){
+        var interestPayment = interestOnly(principle, apr);
+        var calculatedPayments = payments(principle, calculatedDiscountFactor);
+        var percentOfPayment = convertToTwoDecimalSpots((interestPayment * 100)/calculatedPayments)
+    }
+    if ((!notBlank) && (calculatedPayments !== undefined && calculatedPayments !== 'NaN')){
+        document.querySelector("#payments").innerHTML = calculatedPayments;
+        document.querySelector("#length").innerHTML = term;
+        document.querySelector("#interestPayment").innerHTML = interestPayment;
+        document.querySelector("#interestPercent").innerHTML = percentOfPayment;
+        document.querySelector("#results").style.display = "block";
+    } else if ((notBlank === true) || (calculatedPayments === (undefined || 'NaN'))){
+        document.querySelector("#results").style.display= "none";
+
+    }
+}
+
+var principleInput = document.querySelector("input[name=principle]");
+
+principleInput.onfocus = formatPrincipleAsNumber;
+principleInput.onblur = formatPrincipleAsCurrency;
+
+function formatPrincipleAsNumber(){
+        principleInput.type = 'number';
+        if(principle){
+            principleInput.value = Number(
+                parseFloat(principle).toFixed(2));
+        }
+        else{
+            principleInput.value = 0;
+        }
+}
+
+function formatPrincipleAsCurrency() {
+    let options = { style: 'currency', currency: 'USD' };
+    let numberFormat = new Intl.NumberFormat('en-US', options);
+    if(principle){ 
+        principleInput.type = 'text'
+        principleInput.value = numberFormat.format(Number(
+            parseFloat(principle).toFixed(2)))
+        } 
+    else {
+        principleInput.value = numberFormat.format(0)
+        } 
+}
