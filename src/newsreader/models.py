d0b49c29f518a60e20f9f0e2c39203dccdeb9868
Abdinasir <abdinasirnoor@outlook.com>
2020-07-18 22:02:40 -0400

    Adjusted admin view and added blank values for db

--- src/newsreader/models.py
+++ src/newsreader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Article_site(models.Model):
     link_to_site = models.URLField()
-    name = models.CharField(max_length=30, null=True)
+    name = models.CharField(max_length=30)
     language = models.CharField(
         max_length=2,
         choices=[
@@ -23,25 +23,25 @@ class Article_site(models.Model):
             verbose_name_plural = 'Sites to pull article links from'
     
     def __str__(self):
-        return f"{self.name}, language {self.language}"
+        return f"{self.name} - {self.language}"
 class Article_links(models.Model):
     article_link = models.URLField(max_length=255, unique=True) 
     site = models.ForeignKey(Article_site, on_delete=models.CASCADE, verbose_name="article's main site")
-    date_posted = models.DateField(null=True)
-    description = models.CharField(max_length=120, null=True)
-    author = models.CharField(max_length=100, null=True)
-    title = models.CharField(max_length=100, null=True)
+    date_posted = models.DateField(null=True, blank=True)
+    description = models.CharField(max_length=120, blank=True)
+    author = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100, blank=True)
 
     class Meta:
             verbose_name = 'Link to article'
             verbose_name_plural = 'Links to articles'
 
     def __str__(self):
-        return f"{self.title}, from {self.site}"
+        return f"{self.title} from {self.site}"
 class Article_content(models.Model):
     title = models.CharField(max_length=100)
-    date_posted = models.DateField(null=True)
-    author = models.CharField(max_length=100, null=True)
+    date_posted = models.DateField(null=True, blank=True)
+    author = models.CharField(max_length=100, blank=True)
     article_content = models.TextField()
     link_to_content = models.OneToOneField(Article_links, on_delete=models.CASCADE, verbose_name="article's link", unique=True)
     
@@ -50,4 +50,4 @@ class Article_content(models.Model):
             verbose_name_plural = 'Article links to pull article data from'
 
     def __str__(self):
-        return f"{self.title}, from {self.link_to_content.site}"
+        return f"{self.title} from {self.link_to_content.site}"
--- src/personalsite/admin.py
+++ src/personalsite/admin.py
@@ -1,3 +1,8 @@
+from django.apps import apps
+from django.utils.text import capfirst
+from django.utils.translation import gettext as _, gettext_lazy
+from django.template.response import TemplateResponse
+
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 
@@ -25,3 +30,31 @@ class CustomOrderingSite(AdminSite):
             app['models'].sort(key=lambda x: ordering[x['name']] if x['name'] in ordering.keys() else x['name'])
         
         return app_list
+
+
+    def app_index(self, request, app_label, extra_context=None):
+        app_dict = self._build_app_dict(request, app_label)
+        if not app_dict:
+            raise Http404('The requested admin page does not exist.')
+        ordering = {
+                "Sites to pull article links from": 1,
+                "Links to articles": 2,
+                "Article links to pull article data from": 3,
+            }
+        # Sort the models alphabetically within each app.
+        app_dict['models'].sort(key=lambda x: ordering[x['name']] if x['name'] in ordering.keys() else x['name'])
+        app_name = apps.get_app_config(app_label).verbose_name
+        context = {
+            **self.each_context(request),
+            'title': _('%(app)s administration') % {'app': app_name},
+            'app_list': [app_dict],
+            'app_label': app_label,
+            **(extra_context or {}),
+        }
+
+        request.current_app = self.name
+
+        return TemplateResponse(request, self.app_index_template or [
+            'admin/%s/app_index.html' % app_label,
+            'admin/app_index.html'
+        ], context)
