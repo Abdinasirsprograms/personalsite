b59e81e91e5b46fe436aec52d6f80cb2671425d0
Abdinasir <abdinasirnoor@outlook.com>
2021-11-14 16:35:54 -0500

    continuing landingpage 3.0

--- src/static/landingpage/js/header.js
+++ src/static/landingpage/js/header.js
@@ -1,8 +1,7 @@
 const menuButton = document.querySelector(".hamburger_button")
 const animation_classes = ['top_bun_slide_down', 'slide-right', 
     'bottom_bun_slide_up']
-const reverse_animation_classes = ['reverse_top_bun_slide_up', 'reverse_slide-right', 
-    'reverse_bottom_bun_slide_up']
+const link_container = document.querySelector(".link_container")
 const hamburger_svg = document.querySelector(".hamburger")
 
 if (menuButton){
@@ -14,14 +13,26 @@ function openHamburger(e) {
         hamburger = [...hamburger_svg.children]
         hamburger.forEach((e,i) => {
             let class_list = e.classList
+
             if (class_list.contains(animation_classes[i])){
                 // reverse the animation
                 e.getAnimations()[0].playbackRate = -1
                 e.getAnimations()[0].onfinish = function() {
                     class_list.remove(animation_classes[i])
                 }
+                if (i === hamburger.length - 1){
+                    link_container.getAnimations()[0].playbackRate = -1
+                    link_container.getAnimations()[0].onfinish = function() {
+                        link_container.classList.remove("animate_menu")
+                        document.body.style.overflow = null
+                    }
+                }
             } else {
                 class_list.add(animation_classes[i])
-            }
-        });
+                window.scroll({top: 0, left: 0, behavior: 'smooth'})
+                if (i === hamburger.length - 1){
+                    link_container.classList.add("animate_menu")
+                    document.body.style.overflow="hidden"
+                }
+        }});
 }
