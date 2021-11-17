198963d0249395a4ac322a34ab9bfc59e7c4dbc8
Abdinasir <abdinasirnoor@outlook.com>
2021-11-17 18:14:35 -0500

    menu JS bugs

--- src/static/landingpage/js/header.js
+++ src/static/landingpage/js/header.js
@@ -3,6 +3,7 @@ const animation_classes = ['top_bun_slide_down', 'slide-right',
     'bottom_bun_slide_up']
 const link_container = document.querySelector(".link_container")
 const hamburger_svg = document.querySelector(".hamburger")
+const title = document.querySelector("h1")
 
 if (menuButton){
     menuButton.addEventListener('click', openHamburger)
@@ -10,28 +11,43 @@ if (menuButton){
 
 function openHamburger(e) {
     // convert htmlCollection objects to an array
-        hamburger = [...hamburger_svg.children]
-        hamburger.forEach((e,i) => {
-            let class_list = e.classList
-
-            if (class_list.contains(animation_classes[i])){
-                // reverse the animation
-                e.getAnimations()[0].playbackRate = -1
-                e.getAnimations()[0].onfinish = function() {
-                    class_list.remove(animation_classes[i])
+    hamburger = [...hamburger_svg.children]
+    hamburger.forEach((e,i) => {
+        let class_list = e.classList
+        if (class_list.contains(animation_classes[i])){
+            // reverse the animation
+            title.style.animationDelay = "750ms"
+            hamburger_svg.parentNode.style.animationDelay = "750ms"
+            e.getAnimations()[0].playbackRate = -1
+            e.getAnimations()[0].onfinish = function() {
+                class_list.remove(animation_classes[i])
+            }
+            if (i === hamburger.length - 1){
+                link_container.getAnimations()[0].playbackRate = -1
+                link_container.getAnimations()[0].onfinish = function() {
+                    link_container.classList.remove("animate_menu")
+                    document.body.style.overflow = null        
                 }
-                if (i === hamburger.length - 1){
-                    link_container.getAnimations()[0].playbackRate = -1
-                    link_container.getAnimations()[0].onfinish = function() {
-                        link_container.classList.remove("animate_menu")
-                        document.body.style.overflow = null
-                    }
+                hamburger_svg.parentNode.getAnimations()[0].playbackRate = -1
+                title.getAnimations()[0].playbackRate = -1
+                hamburger_svg.parentNode.getAnimations()[0].onfinish = function(){
+                    title.style.animationDelay = null
+                    hamburger_svg.parentNode.style.animationDelay = null
+                    hamburger_svg.parentNode.classList.remove("active_menu")
+                    title.classList.remove("active_menu");
                 }
-            } else {
-                class_list.add(animation_classes[i])
-                if (i === hamburger.length - 1){
-                    link_container.classList.add("animate_menu")
-                    document.body.style.overflow="hidden"
-                }
-        }});
+            }
+        } else {
+            class_list.add(animation_classes[i])
+            if (i === 0){
+                title.style.animationDelay = null
+                hamburger_svg.parentNode.style.animationDelay = null
+                hamburger_svg.parentNode.classList.add("active_menu")
+                title.classList.add("active_menu");
+            }
+            if (i === hamburger.length - 1){
+                link_container.classList.add("animate_menu")
+                document.body.style.overflow="hidden"
+            }
+    }})
 }
