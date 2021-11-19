5786f057df5d9e44c3a7143968dd3e52527d6d9a
Abdinasir <abdinasirnoor@outlook.com>
2021-11-18 23:42:26 -0500

    header bug fixed

--- src/static/landingpage/js/header.js
+++ src/static/landingpage/js/header.js
@@ -4,20 +4,13 @@ const animation_classes = ['top_bun_slide_down', 'slide-right',
 const link_container = document.querySelector(".link_container")
 const hamburger_svg = document.querySelector(".hamburger")
 const title = document.querySelector("h1")
+const hamburger = [...hamburger_svg.children]
 
-if (menuButton){
-    menuButton.addEventListener('click', openHamburger)
-}
-
-function openHamburger(e) {
-    // convert htmlCollection objects to an array
-    hamburger = [...hamburger_svg.children]
+function animateHamburger(e){
     hamburger.forEach((e,i) => {
         let class_list = e.classList
         if (class_list.contains(animation_classes[i])){
             // reverse the animation
-            title.style.animationDelay = "750ms"
-            hamburger_svg.parentNode.style.animationDelay = "750ms"
             e.getAnimations()[0].playbackRate = -1
             e.getAnimations()[0].onfinish = function() {
                 class_list.remove(animation_classes[i])
@@ -31,23 +24,34 @@ function openHamburger(e) {
                 hamburger_svg.parentNode.getAnimations()[0].playbackRate = -1
                 title.getAnimations()[0].playbackRate = -1
                 hamburger_svg.parentNode.getAnimations()[0].onfinish = function(){
-                    title.style.animationDelay = null
-                    hamburger_svg.parentNode.style.animationDelay = null
                     hamburger_svg.parentNode.classList.remove("active_menu")
                     title.classList.remove("active_menu");
                 }
             }
         } else {
             class_list.add(animation_classes[i])
-            if (i === 0){
-                title.style.animationDelay = null
-                hamburger_svg.parentNode.style.animationDelay = null
-                hamburger_svg.parentNode.classList.add("active_menu")
-                title.classList.add("active_menu");
-            }
             if (i === hamburger.length - 1){
                 link_container.classList.add("animate_menu")
                 document.body.style.overflow="hidden"
             }
     }})
+}
+
+if (menuButton){
+    menuButton.addEventListener('click', openHamburger)
+}
+
+function openHamburger(e) {
+    // convert htmlCollection objects to an array
+    if (hamburger_svg.parentNode.classList.contains("active_menu")){
+        title.style.animationDuration = "1.5s"
+        hamburger_svg.parentNode.style.animationDuration = "1.5s"
+        animateHamburger(e)
+    } else {
+        title.style.animationDuration = "750ms"
+        hamburger_svg.parentNode.style.animationDuration = "750ms"
+        hamburger_svg.parentNode.classList.add("active_menu")
+        title.classList.add("active_menu");
+        animateHamburger(e)
+    }
 }
