232c9401992089a3b1a321d4316e9a8fd69d0fb2
Abdinasir <abdinasirnoor@outlook.com>
2021-11-22 15:37:33 -0500

    header is done, added hero area assets to index

--- src/static/landingpage/js/header.js
+++ src/static/landingpage/js/header.js
@@ -1,9 +1,10 @@
 const menuButton = document.querySelector(".hamburger_button")
 const animation_classes = ['top_bun_slide_down', 'slide-right', 
-    'bottom_bun_slide_up']
+'bottom_bun_slide_up']
 const link_container = document.querySelector(".link_container")
 const hamburger_svg = document.querySelector(".hamburger")
 const title = document.querySelector("h1")
+// convert htmlCollection objects to an array
 const hamburger = [...hamburger_svg.children]
 
 function animateHamburger(e){
@@ -19,7 +20,8 @@ function animateHamburger(e){
                 link_container.getAnimations()[0].playbackRate = -1
                 link_container.getAnimations()[0].onfinish = function() {
                     link_container.classList.remove("animate_menu")
-                    document.body.style.overflow = null        
+                    document.body.style.overflow = null      
+                    link_container.style.display = "none"
                 }
                 hamburger_svg.parentNode.getAnimations()[0].playbackRate = -1
                 title.getAnimations()[0].playbackRate = -1
@@ -38,20 +40,21 @@ function animateHamburger(e){
 }
 
 if (menuButton){
+    link_container.style.display = "none"
     menuButton.addEventListener('click', openHamburger)
 }
 
 function openHamburger(e) {
-    // convert htmlCollection objects to an array
     if (hamburger_svg.parentNode.classList.contains("active_menu")){
         title.style.animationDuration = "1.5s"
         hamburger_svg.parentNode.style.animationDuration = "1.5s"
         animateHamburger(e)
-    } else {
-        title.style.animationDuration = "750ms"
-        hamburger_svg.parentNode.style.animationDuration = "750ms"
-        hamburger_svg.parentNode.classList.add("active_menu")
-        title.classList.add("active_menu");
-        animateHamburger(e)
-    }
+        return
+    } 
+    link_container.style.display = null
+    title.style.animationDuration = "750ms"
+    hamburger_svg.parentNode.style.animationDuration = "750ms"
+    hamburger_svg.parentNode.classList.add("active_menu")
+    title.classList.add("active_menu");
+    animateHamburger(e)
 }
